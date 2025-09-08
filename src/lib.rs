320eb09cd26e69a1923c507f857b267117c60e9a
ajdelcimmuto <ajdelco22@gmail.com>
2025-09-08 17:16:05 -0500

    adding a wasmhttpclient

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,77 @@ pub fn set_url(url: &str) {
 }
 
 #[wasm_bindgen]
+pub struct WasmHttpClient {
+    base_url: Option<String>,
+    default_headers: Headers,
+    timeout_ms: u32
+}
+
+pub struct WasmResponse {
+    pub status: u16,
+    pub status_text: String,
+    pub headers: Headers,
+    pub body: String,
+    pub ok: bool,
+}
+
+impl WasmHttpClient {
+    pub fn new() -> Self {
+        Self {
+            base_url: None,
+            default_headers: Headers::new().unwrap(),
+            timeout_ms: 10000
+        }
+    }
+
+    pub fn with_base_url(&mut self, base_url: &str) {
+        self.base_url = Some(base_url.to_string());
+    }
+
+    pub fn add_default_header(&mut self, key: &str, value: &str) {
+        self.default_headers.append(key, value).unwrap();
+    }
+
+    pub fn set_timeout(&mut self, timeout_ms: u32) {
+        self.timeout_ms = timeout_ms;
+    }
+
+    pub async fn get(&self) -> Result<WasmResponse, JsValue> {
+        let opts = RequestInit::new();
+        opts.set_method("GET");
+
+        opts.set_headers(self.default_headers.as_ref());
+
+        let url = match &self.base_url {
+            Some(base) => base.as_str(),
+            None => return Err(JsValue::from_str("No base URL set")),
+        };
+
+        let request = Request::new_with_str_and_init(url, &opts)?;
+        let window = web_sys::window().unwrap();
+
+        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+        let resp: web_sys::Response = resp_value.dyn_into()?;
+
+        if !resp.ok() {
+            return Err(JsValue::from_str(&format!("HTTP {}: {}",
+                resp.status(), resp.status_text())));
+        }
+
+        let text = JsFuture::from(resp.text()?).await?;
+        let text_string = text.as_string().unwrap();
+
+        return Ok(WasmResponse {
+            status: resp.status(),
+            status_text: resp.status_text(),
+            headers: resp.headers(),
+            body: text_string,
+            ok: true
+        })
+    }
+}
+
+#[wasm_bindgen]
 pub struct StreamingClient {
     base_url: String
 }
@@ -39,32 +110,16 @@ impl StreamingClient {
     pub async fn fetch_manifest(&mut self) -> Result<String, JsValue> {
         log::info!("fetch_manifest");
 
-        let opts = RequestInit::new();
-        opts.set_method("GET");
+        let mut wasm_http_client = WasmHttpClient::new();
+        wasm_http_client.with_base_url(&self.base_url);
+        wasm_http_client.add_default_header("Accept", "application/vnd.apple.mpegurl");
+        let master_playlist_response: WasmResponse = wasm_http_client.get().await?;
 
-        let headers = Headers::new()?;
-        headers.set("Accept", "application/vnd.apple.mpegurl")?;
-        opts.set_headers(&headers);
-
-        let request = Request::new_with_str_and_init(&self.base_url, &opts)?;
-        let window = web_sys::window().unwrap();
-
-        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-        let resp: web_sys::Response = resp_value.dyn_into()?;
-
-        if !resp.ok() {
-            return Err(JsValue::from_str(&format!("HTTP {}: {}",
-                resp.status(), resp.status_text())));
-        }
-
-        let text = JsFuture::from(resp.text()?).await?;
-        let text_string = text.as_string().unwrap();
-
-        let master_playlist = self.parse_manifest(text_string).unwrap();
+        let master_playlist = self.parse_manifest(master_playlist_response.body).unwrap();
 
         match &master_playlist {
             Playlist::MasterPlaylist(pl) => {
-                let media_playlist = self.fetch_media_playlist(pl).await?;
+                self.fetch_media_playlist(pl).await?;
             },
             _ => {}
         }
@@ -88,35 +143,17 @@ impl StreamingClient {
     async fn fetch_media_playlist(&mut self, master_playlist: &MasterPlaylist) -> Result<String, JsValue> {
         log::info!("fetch_media_playlist");
 
-        let opts = RequestInit::new();
-        opts.set_method("GET");
-
-        let headers = Headers::new()?;
-        headers.set("Accept", "application/vnd.apple.mpegurl")?;
-        opts.set_headers(&headers);
-
-        // Fetch first variant for POC purposes
         let base_url = "https://stream-fastly.castr.com/5b9352dbda7b8c769937e459/live_2361c920455111ea85db6911fe397b9e/";
-        let url = format!("{}{}", base_url.to_string(), master_playlist.variants[0].uri);
-        let request = Request::new_with_str_and_init(url.as_str(), &opts)?;
-        let window = web_sys::window().unwrap();
+        let mut wasm_http_client = WasmHttpClient::new();
+        wasm_http_client.with_base_url(format!("{}{}", base_url.to_string(), master_playlist.variants[0].uri).as_str());
+        wasm_http_client.add_default_header("Accept", "application/vnd.apple.mpegurl");
+        let media_playlist_response: WasmResponse = wasm_http_client.get().await?;
 
-        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-        let resp: web_sys::Response = resp_value.dyn_into()?;
-
-        if !resp.ok() {
-            return Err(JsValue::from_str(&format!("HTTP {}: {}",
-                resp.status(), resp.status_text())));
-        }
-
-        let text = JsFuture::from(resp.text()?).await?;
-        let text_string = text.as_string().unwrap();
-
-        let media_playlist = self.parse_manifest(text_string).unwrap();
+        let media_playlist = self.parse_manifest(media_playlist_response.body).unwrap();
 
         match &media_playlist {
             Playlist::MediaPlaylist(pl) => {
-                let media_playlist = self.fetch_media_segment(pl).await?;
+                self.fetch_media_segment(pl).await?;
             },
             _ => {}
         }
